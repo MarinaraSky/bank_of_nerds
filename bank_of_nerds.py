0670849b849006613c960718a3c88e256fe31718
Jack Spence <jack.c.spence@gmail.com>
2018-11-08 19:58:26 -0500

    Added login feature after account has been created.

--- bank_of_nerds.py
+++ bank_of_nerds.py
@@ -1,12 +1,19 @@
 #Bank of nerds MAIN
 # Change * h
 from bon_Classes import Account, Customer
+from getpass import getpass
 
 def main():
-    login_options = {'1': None, '2': new_customer}
-    login_menu = "1) Existing Customer.\n2) New User.\n>"
-    selection = login_options[my_input(login_menu)]
-    selection()
+
+    loop = True
+    while loop == True:
+        login_options = {'1': user_login, '2': new_customer, '9': exit}
+        login_menu = "1) Existing Customer.\n2) New User.\n9) Quit.\n>"
+        try:
+            selection = login_options[my_input(login_menu)]
+            loop = selection()
+        except KeyError:
+            print("Not a Valid option.")
 
 
 def new_customer():
@@ -21,6 +28,28 @@ def new_customer():
     age = my_input(age_prompt)
 
     new = Customer(first_name, last_name, username, age)
+    if new:
+        #Loop Variable
+        return True
+    else:
+        return False
+
+def user_login():
+    login_prompt = "Please enter your user ID.\n>"
+    password_prompt = "Please enter your password.\n>"
+    error_prompt = "Cannot log in.\n"
+    login_attempt = my_input(login_prompt)
+    try:
+        if Customer.customers[login_attempt].password == hash(getpass(password_prompt)):
+            print("Success")
+            account_management(Customer.customers[login_attempt])
+        else:
+            print("Fail")
+    except KeyError:
+        print(error_prompt)
+        return False
+
+
 
 def my_input(prompt):
     while True:
