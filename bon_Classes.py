c6a7ff5a592d5b7e48a12a4d294a76ea3ae52f5a
Jack Spence <jack.c.spence@gmail.com>
2018-11-09 18:13:00 -0500

    Added docstrings to classes and functions.

--- bon_Classes.py
+++ bon_Classes.py
@@ -3,7 +3,11 @@ import hashlib
 
 
 class Account:
+    '''Base Account class from which Savings, Checkings, and 401k
+    Accounts will be structured from'''
     def __init__(self, account_num):
+        '''Takes the account_num from the respective sub class
+        and uses it when creating an account'''
         self.account_id = account_num
         self.balance = 0
         self.withdraws = 0
@@ -11,6 +15,8 @@ class Account:
         self.interest = 0
 
     def withdraw(self, amount):
+        '''Takes the amount of money to withdraw and will
+        make the new balance reflect that change'''
         if amount < 0:
             print("Can't withdraw negative monies.")
             return
@@ -24,6 +30,8 @@ class Account:
             self.withdraws += 1
 
     def deposit(self, amount):
+        '''Takes the amount of money to deposit and will
+        make the new balance reflect that change'''
         if amount < 0:
             print("Can't deposit negative monies.")
             return
@@ -31,6 +39,8 @@ class Account:
         self.deposits += 1
 
     def __str__(self, type_of_account):
+        '''Takes the type of account and returns
+        the proper string representation for an account'''
         acc_string = "{0} account: {1} \n   Balance: ${2:.2f}\n   " \
                      "Interest: {3}%\n   Withdraws: {4}\n   " \
                      "Deposits: {5}".format(
@@ -41,47 +51,61 @@ class Account:
 
 
 class Checking(Account):
+    '''Subclass for Checking account.'''
     checking_id = 10000000
 
     def __init__(self):
+        '''Creates unique id'd checking account'''
         super().__init__(Checking.checking_id)
         print("Checking", Checking.checking_id)
         Checking.checking_id += 1
 
     def __str__(self):
+        '''Sends Account.__str__ the Checking string for printing'''
         return super().__str__("Checking")
 
 
 class Savings(Account):
+    '''Subclass for Savings account.'''
     savings_id = 10000000
 
     def __init__(self):
+        '''Creates unique id'd savings account'''
         super().__init__(Savings.savings_id)
         print("Savings", Savings.savings_id)
         Savings.savings_id += 1
         self.interest = 0.05
 
     def __str__(self):
+        '''Sends Account.__str__ the Savings string for printing'''
         return super().__str__("Savings")
 
 
 class FourOhOneK(Account):
+    '''Subclass for 401k account.'''
     four_oh_one_k_id = 10000000
 
     def __init__(self):
+        '''Creates unique id'd 401k account'''
         super().__init__(FourOhOneK.four_oh_one_k_id)
         FourOhOneK.four_oh_one_k_id += 1
         self.interest = 0.05
 
     def __str__(self):
+        '''Sends Account.__str__ the 401k string for printing'''
         return super().__str__("401k")
 
 
 class Customer:
+    '''Class used to represent a customer'''
     customer_id = 1000
+    '''Customers is a dictionary of Customer(), keys are usernames
+    and values are the Customer() objects themselves'''
     customers = dict()
 
     def __init__(self, first_name, last_name, username, age):
+        '''Takes first_name, last_name, username, and age and will
+        prompt the user for passwords that are stored as md5 sum hashes.'''
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
@@ -96,6 +120,7 @@ class Customer:
         Customer.customer_id += 1
 
     def get_password(self):
+        '''Instance method that will prompt the user for a password'''
         pw_prompt = "Please enter a password.\n>"
         pw_confirm = "Please confirm your password.\n>"
         while True:
@@ -115,6 +140,7 @@ class Customer:
                 pass
 
     def __str__(self):
+        '''String representation of a Customer()'''
         output = "Name: {} {}\nAge: {}\nId: {}"
         return output.format(
                 self.first_name, self.last_name, self.age, self.id)
