0a3e54f7f0eae5273989680f377451858320553e
Jack Spence <jack.c.spence@gmail.com>
2018-11-08 21:17:58 -0500

    Began to add menu to account management. Added functionality to add checking and savings, as well as deposit into both.

--- bank_of_nerds.py
+++ bank_of_nerds.py
@@ -1,19 +1,25 @@
 #Bank of nerds MAIN
 # Change * h
-from bon_Classes import Account, Customer
+from bon_Classes import Checking, Savings, Customer
 from getpass import getpass
 
 def main():
 
     loop = True
-    while loop == True:
+    while loop:
         login_options = {'1': user_login, '2': new_customer, '9': exit}
         login_menu = "1) Existing Customer.\n2) New User.\n9) Quit.\n>"
         try:
             selection = login_options[my_input(login_menu)]
             loop = selection()
+            if isinstance(loop, Customer):
+                break
         except KeyError:
             print("Not a Valid option.")
+    customer = loop
+    loop = True
+    while loop:
+        loop = account_management(customer)
 
 
 def new_customer():
@@ -42,14 +48,54 @@ def user_login():
     try:
         if Customer.customers[login_attempt].password == hash(getpass(password_prompt)):
             print("Success")
-            account_management(Customer.customers[login_attempt])
+            return Customer.customers[login_attempt]
         else:
             print("Fail")
     except KeyError:
         print(error_prompt)
         return False
 
+def account_management(selected_customer):
+    account_prompt = "1) New Checking\n2) New Savings\n3) Deposit Checking\n" \
+            "4) Savings Deposit\n9) Exit\n>"
+    account_input = my_input(account_prompt)
+    if account_input == "9":
+        return False
+    elif account_input == "1":
+        selected_customer.accounts['Checking'].update({Checking.checking_id: Checking()})
+    elif account_input == "2":
+        selected_customer.accounts['Savings'].update({Savings.savings_id: Savings()})
+    elif account_input == "3":
+        checking_deposit(selected_customer)
+    elif account_input == "4":
+        savings_deposit(selected_customer)
+    return True
 
+def checking_deposit(customer):
+    list_accounts(customer.accounts['Checking'])
+    try:
+        customer.accounts['Checking'][selected].deposit(amount)
+    except KeyError:
+        print("Cannont find that account.")
+
+def savings_deposit(customer):
+    list_accounts(customer.accounts['Savings'])
+    try:
+        customer.accounts['Savings'][selected].deposit(amount)
+    except KeyError:
+        print("Cannont find that account.")
+
+def list_accounts(customer_accounts):
+    account_prompt = "Which account? "
+    amount_prompt = "How much money? "
+    for account in customer_accounts:
+        print(account)
+    selected_account = my_input(account_prompt)
+    amount = my_input(amount_prompt)
+    try:
+        return int(selected_account), float(amount)
+    except ValueError:
+        return None, None
 
 def my_input(prompt):
     while True:
